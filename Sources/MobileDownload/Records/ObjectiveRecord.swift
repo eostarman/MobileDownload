350ac6e12cd910fbb232c85c6b6473609d1beb80
Michael Rutherford <doomiigo@gmail.com>
2021-07-09 16:36:01 -0400

    Added UI to see objectives (also, added a top-level button to show *all* objectives that are still open (still to-do). Added a new CancellationButton - it is used to cancel a task (it includes a spinner - progressView - and a caption of "cancel" Cleaned up the mobile download decoder for the ObjectiveRecord and added an ObjectiveService (which just loads objectives at the moment)

--- Sources/MobileDownload/Records/ObjectiveRecord.swift
+++ Sources/MobileDownload/Records/ObjectiveRecord.swift
@@ -1,27 +1,27 @@
 import Foundation
 
-public final class ObjectiveRecord: Record, Codable {
+public final class ObjectiveRecord: Record, Identifiable, Codable {
     public var id: Int { recNid }
     public var recNid: Int = 0
     public var recKey: String = ""
     public var recName: String = ""
 
     public var enteredByEmpNid: Int = 0
-    public var entryTime: String = "" // DateTime
-    public var dueByDate: String = "" // DateTime
-    public var doneTime: String = "" // DateTime
-    public var doneSuccess: Int = 0
-    public var doneComment: String = ""
-    public var cusNid: Int?
+    public var entryTime: Date = Date()
+    public var dueByDate: Date = Date()
+    public var doneTime: Date?
+    public var doneSuccess: Bool?
+    public var doneComment: String?
+    public var cusNid: Int = 0
     public var supNid: Int?
-    public var objectiveTypeNid: Int = 0
+    public var objectiveTypeNid: Int?
     public var hostWhseNid: Int = 0
     public var empNid: Int = 0
     public var description: String = ""
     public var supplierRecName: String = ""
     public var enteredByRecName: String = ""
     public var tempNid: Int?
-    public var startDate: Date = .distantPast
+    public var startDate: Date?
     public var pkgNid: Int?
     public var brandNid: Int?
     public var itemNid: Int?
